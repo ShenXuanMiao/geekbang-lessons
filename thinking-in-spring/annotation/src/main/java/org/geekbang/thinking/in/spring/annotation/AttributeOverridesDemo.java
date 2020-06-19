bbd53e5ffbb5069ad14060a513fbf884d54fa931
xuan <601769037@qq.com>
2020-06-19 11:44:20 +0800

    1去掉代码中license说明 2修改generic模块依赖引用问题 3去除javadoc中@since 4修复ioc-container-overview模块中的一部分Warning

--- thinking-in-spring/annotation/src/main/java/org/geekbang/thinking/in/spring/annotation/AttributeOverridesDemo.java
+++ thinking-in-spring/annotation/src/main/java/org/geekbang/thinking/in/spring/annotation/AttributeOverridesDemo.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.geekbang.thinking.in.spring.annotation;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -22,7 +7,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * Spring 注解属性覆盖示例
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
- * @since
+ * 
  */
 @MyComponentScan2(packages = "org.geekbang.thinking.in.spring.annotation")
 // @MyComponentScan2.scanBasePackages  <- @MyComponentScan.scanBasePackages 隐性覆盖
